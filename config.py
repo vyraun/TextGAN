396b908ef4369a1e61411b225096061bda67a09b
Ankit Vani <a@nevitus.org>
2016-10-29 02:29:32 -0400

    update default configs

--- config.py
+++ config.py
@@ -15,14 +15,14 @@ flags.DEFINE_integer("word_emb_size",     224,     "Number of learnable dimensio
                                                    "embeddings")
 flags.DEFINE_integer("num_layers",        2,       "Number of RNN layers")
 flags.DEFINE_integer("hidden_size",       192,     "RNN hidden state size")
-flags.DEFINE_float  ("word_dropout",      0.33,    "Word dropout probability")
+flags.DEFINE_float  ("word_dropout",      0.25,    "Word dropout probability")
 flags.DEFINE_integer("softmax_samples",   1000,    "Number of classes to sample for softmax")
 flags.DEFINE_integer("generator_top_k",   1,       "Number of words to consider from previous " \
                                                    "timestep during generation (-1 for all)")
 flags.DEFINE_float  ("maintain_d_acc",    0.95,    "Try to maintain descriminator accuracy to " \
                                                    "this value (accross prints)")
 flags.DEFINE_float  ("d_acc_slack",       0.03,    "Let descriminator accuracy vary by this much")
-flags.DEFINE_float  ("max_perplexity",    12.0,    "Scheduler maintains perplexity to be under " \
+flags.DEFINE_float  ("max_perplexity",    15.0,    "Scheduler maintains perplexity to be under " \
                                                    "this")
 flags.DEFINE_integer("gen_sent_length",   96,      "Maximum length of a generated sentence")
 flags.DEFINE_float  ("max_grad_norm",     5.0,     "Gradient clipping")
